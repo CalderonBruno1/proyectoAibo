e54a7cdf33ed035fae687ca30d7bb1654dfb9a5a
JeanCa1993 <jean.atalaya@pucp.edu.pe>
2022-09-28 11:52:51 -0500

    Agregue Activity Simulacion Prestamo

---
+++ app/src/main/java/com/upc/aiboapp/SimulacionActivity.kt
@@ -0,0 +1,11 @@
+package com.upc.aiboapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimulacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simulacion)
+    }
+}
